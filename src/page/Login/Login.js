67e4771a2d0c14cc18174dee7356fbc8d6987c38
FlaSh <nexus2493@gmail.com>
2020-11-15 08:27:54 +0900

    로그인 버튼 margin 수정

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
         </Col>
         <Col md={{span: 8, offset: 8}}>
             <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
+                <Button type="primary" htmlType="submit" className="login-form-button" style={{marginRight: "10px"}}>
                 Log in
                 </Button>
                 Or <a href="">register now!</a>
