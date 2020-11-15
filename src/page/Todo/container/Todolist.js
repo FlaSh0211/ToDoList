fcb3e0431e8292bdda9910c8d6cc07c5f48d419f
FlaSh <nexus2493@gmail.com>
2020-11-16 07:31:24 +0900

    react-infinite-scroller 설치

--- src/page/Todo/container/Todolist.js
+++ src/page/Todo/container/Todolist.js
@@ -1,13 +1,70 @@
 import React from 'react';
 import styled from "styled-components"
-import Wrapper from 'component/Wrapper/Wrapper'
 import 'antd/dist/antd.css'
+import { List, message, Avatar, Spin } from 'antd';
+import  from 'react-infinite-scroller';
 
 const Todolist = () => {
+    const initialstate = {
+        data: [],
+        loading: false,
+        hasMore: true,
+      };
+
+    const handleInfiniteOnLoad = () => {
+        let { data } = this.state;
+        this.setState({
+          loading: true,
+        });
+        if (data.length > 14) {
+          message.warning('Infinite List loaded all');
+          this.setState({
+            hasMore: false,
+            loading: false,
+          });
+          return;
+        }
+        this.fetchData(res => {
+          data = data.concat(res.results);
+          this.setState({
+            data,
+            loading: false,
+          });
+        });
+      };
+
     return (
-        <div>
-            투두리스트
-        </div>
+        <div className="demo-infinite-container">
+            <InfiniteScroll
+            initialLoad={false}
+            pageStart={0}
+            loadMore={this.handleInfiniteOnLoad}
+            hasMore={!this.state.loading && this.state.hasMore}
+            useWindow={false}
+            >
+            <List
+                dataSource={this.state.data}
+                renderItem={item => (
+                <List.Item key={item.id}>
+                    <List.Item.Meta
+                    avatar={
+                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    }
+                    title={<a href="https://ant.design">{item.name.last}</a>}
+                    description={item.email}
+                    />
+                    <div>Content</div>
+                </List.Item>
+                )}
+            >
+                {this.state.loading && this.state.hasMore && (
+                <div className="demo-loading-container">
+                    <Spin />
+                </div>
+                )}
+            </List>
+            </InfiniteScroll>
+      </div>
     )
 }
 
