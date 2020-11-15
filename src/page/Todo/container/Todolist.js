2e55ff1b6c407db6792ce17024bf2b389aa3e978
FlaSh <nexus2493@gmail.com>
2020-11-16 08:50:49 +0900

    홈화면에 달력 추가

--- src/page/Todo/container/Todolist.js
+++ src/page/Todo/container/Todolist.js
@@ -1,60 +1,34 @@
 import React, { useState } from 'react';
 import styled from "styled-components"
 import 'antd/dist/antd.css'
-import { List, message, Avatar, Spin } from 'antd';
-import InfiniteScroll from 'react-infinite-scroller';
+import { List, message, Avatar, Spin, Col } from 'antd';
 
 const Todolist = () => {
     const [state, setState] = useState({
-        data: [],
+        data: [{id: 1, date:'2020.11.16', title: "today to do"}],
         loading: false,
         hasMore: true,
       });
 
-    const handleInfiniteOnLoad = () => {
-        let { data } = this.state;
-        setState({
-          loading: true,
-        });
-        if (data.length > 14) {
-          message.warning('Infinite List loaded all');
-          setState({
-            hasMore: false,
-            loading: false,
-          });
-          return;
-        }
-        // fetchData(res => {
-        //   data = data.concat(res.results);
-        //   this.setState({
-        //     data,
-        //     loading: false,
-        //   });
-        // });
-      };
-
     return (
         <div className="demo-infinite-container">
-            <InfiniteScroll
-            initialLoad={false}
-            pageStart={0}
-            loadMore={handleInfiniteOnLoad}
-            hasMore={!state.loading && state.hasMore}
-            useWindow={false}
-            >
             <List
                 dataSource={state.data}
                 renderItem={item => (
-                <List.Item key={item.id}>
-                    <List.Item.Meta
-                    avatar={
-                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                    }
-                    title={<a href="https://ant.design">{item.name.last}</a>}
-                    description={item.email}
-                    />
-                    <div>Content</div>
-                </List.Item>
+                <div style={{marginTop: "80px"}}>
+                    <Col md={{span: 12, offset:6}}>
+                        <List.Item key={item.id} style={{marginLeft: "10px", marginRight: "10px"}}>
+                            <List.Item.Meta
+                            avatar={
+                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                            }
+                            title={<a href="https://ant.design">{item.title}</a>}
+                            description={item.date}
+                            />
+                            <div>Content</div>
+                        </List.Item>
+                    </Col>
+                </div>
                 )}
             >
                 {state.loading && state.hasMore && (
@@ -63,7 +37,6 @@ const Todolist = () => {
                 </div>
                 )}
             </List>
-            </InfiniteScroll>
       </div>
     )
 }
