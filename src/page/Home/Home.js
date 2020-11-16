b9fced01cc83c5cc88cdd665ba4c03f125952b5f
FlaSh <nexus2493@gmail.com>
2020-11-16 13:09:14 +0900

    달력 리스트 기능 만들기 목업 1

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,68 +1,58 @@
-import React from 'react';
-import styled from "styled-components"
-import Wrapper from 'component/Wrapper/Wrapper'
+import React, { useState } from 'react';
 import 'antd/dist/antd.css'
 import { Calendar, Badge } from 'antd';
-function getListData(value) {
-    let listData;
-    switch (value.date()) {
-      case 8:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-        ];
-        break;
-      case 10:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-          { type: 'error', content: 'This is error event.' },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: 'warning', content: 'This is warning event' },
-          { type: 'success', content: 'This is very long usual event。。....' },
-          { type: 'error', content: 'This is error event 1.' },
-          { type: 'error', content: 'This is error event 2.' },
-          { type: 'error', content: 'This is error event 3.' },
-          { type: 'error', content: 'This is error event 4.' },
-        ];
-        break;
-      default:
-    }
-    return listData || [];
-  }
 
-  function dateCellRender(value) {
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map(item => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  
-  function getMonthData(value) {
-    if (value.month() === 8) {
-      return 1394;
-    }
-  }
-  
-  function monthCellRender(value) {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
-  }
+
 const Home = () => {
+    function getListData(value) {   
+        const data = [[2020,0,1],[2021,1,1],[2020,3,1]];
+        let listData;
+        for (let v = 0; v < data.length; v++){
+            if(value.year() === Number(data[v][0])){
+                if(value.month() === Number(data[v][1])) {
+                    if(value.date() === Number(data[v][2])){
+                        listData = [
+                            { type: 'warning', content: 'This is warning event.' },
+                            { type: 'success', content: 'This is usual event.' },
+                        ];
+                    }
+                }
+            }
+        }
+        return listData || [];
+    
+      }
+    
+      function dateCellRender(value) {
+        const listData = getListData(value);
+        const moth = getMonthData(value);
+        console.log(moth, "dfdf")
+        return (
+          <ul className="events">
+            {listData.map(item => (
+              <li key={item.content}>
+                <Badge status={item.type} text={item.content} />
+              </li>
+            ))}
+          </ul>
+        );
+      }
+      
+      function getMonthData(value) {
+        if (value.month() === 8) {
+          return 1394;
+        }
+      }
+      
+      function monthCellRender(value) {
+        const num = getMonthData(value);
+        return num ? (
+          <div className="notes-month">
+            <section>{num}</section>
+            <span>Backlog number</span>
+          </div>
+        ) : null;
+      }
     return (
         <div style={{margin:"20px"}}>
             <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
