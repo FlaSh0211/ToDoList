0bf3ae2c929e9bce935362138a99331572ce28fc
FlaSh <nexus2493@gmail.com>
2020-11-16 13:42:15 +0900

    메인페이지 달력 기능 완성

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,58 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css'
 import { Calendar, Badge } from 'antd';
 
 
 const Home = () => {
     function getListData(value) {   
-        const data = [[2020,0,1],[2021,1,1],[2020,3,1]];
-        let listData;
+        const data = [
+            [2020,0,1,{ type: 'warning', content: 'This is warning event.' }],
+            [2020,0,1,{ type: 'success', content: 'This is success event.' }],
+            [2020,3,1,{ type: 'error', content: 'This is error event.' }]
+        ];
+        let listData=[];
         for (let v = 0; v < data.length; v++){
             if(value.year() === Number(data[v][0])){
                 if(value.month() === Number(data[v][1])) {
                     if(value.date() === Number(data[v][2])){
-                        listData = [
-                            { type: 'warning', content: 'This is warning event.' },
-                            { type: 'success', content: 'This is usual event.' },
-                        ];
+                        listData = listData.concat(data[v][3])
                     }
                 }
             }
         }
         return listData || [];
-    
-      }
-    
-      function dateCellRender(value) {
-        const listData = getListData(value);
-        const moth = getMonthData(value);
-        console.log(moth, "dfdf")
-        return (
-          <ul className="events">
+  }
+
+  function dateCellRender(value) {
+    const listData = getListData(value);
+    return (
+        <ul className="events">
             {listData.map(item => (
-              <li key={item.content}>
+                <li key={item.content} style={{listStyle: "none"}}>
                 <Badge status={item.type} text={item.content} />
-              </li>
+                </li>
             ))}
-          </ul>
-        );
-      }
-      
-      function getMonthData(value) {
-        if (value.month() === 8) {
-          return 1394;
-        }
-      }
-      
-      function monthCellRender(value) {
-        const num = getMonthData(value);
-        return num ? (
-          <div className="notes-month">
-            <section>{num}</section>
-            <span>Backlog number</span>
-          </div>
-        ) : null;
-      }
+        </ul>
+    );
+  }
+  
+  function getMonthData(value) {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  }
+  
+  function monthCellRender(value) {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  }
     return (
         <div style={{margin:"20px"}}>
             <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
