dbc6b301441fb0487718227cc9a4d2087a1eb6f1
FlaSh <nexus2493@gmail.com>
2020-11-19 11:47:22 +0900

    회원가입 처리 기능 구현

--- src/component/Form/Form.js
+++ src/component/Form/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Tooltip, Select, Button } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 const { Option } = Select;
@@ -40,131 +40,120 @@ const tailFormItemLayout = {
 };
 
 const RegisterForm = () => {
-  const [form] = Form.useForm();
+    const [form] = Form.useForm();
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: "",
+        nickname: ""
+    });
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
+    const onChange = e => {
+        setInputs({...inputs, [e.target.name]: e.target.value})
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        let data = {
+            email: inputs.email,
+            password: inputs.password,
+            nickname: inputs.nickname
+        };
+        console.log(data,'데이터');
 
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select
-        style={{
-          width: 70,
+    }
+
+    return (
+        <Form style={{marginTop: "80px"}}
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        initialValues={{
+            residence: ['zhejiang', 'hangzhou', 'xihu'],
+            prefix: '86',
         }}
-      >
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    </Form.Item>
-  );
-
-  return (
-    <Form style={{marginTop: "80px"}}
-      {...formItemLayout}
-      form={form}
-      name="register"
-      onFinish={onFinish}
-      initialValues={{
-        residence: ['zhejiang', 'hangzhou', 'xihu'],
-        prefix: '86',
-      }}
-      scrollToFirstError
-    >
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: 'email',
-            message: 'The input is not valid E-mail!',
-          },
-          {
-            required: true,
-            message: 'Please input your E-mail!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject('The two passwords that you entered do not match!');
+        scrollToFirstError
+        onSubmit = {handleSubmit}
+        >
+        <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+            {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
             },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        name="nickname"
-        label={
-          <span>
-            Nickname&nbsp;
-            <Tooltip title="What do you want others to call you?">
-              <QuestionCircleOutlined />
-            </Tooltip>
-          </span>
-        }
-        rules={[
-          {
-            required: true,
-            message: 'Please input your nickname!',
-            whitespace: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="phone"
-        label="Phone Number"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your phone number!',
-          },
-        ]}
-      >
-        <Input
-          addonBefore={prefixSelector}
-          style={{
-            width: '100%',
-          }}
-        />
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
-  );
+            {
+                required: true,
+                message: 'Please input your E-mail!',
+            },
+            ]}
+        >
+            <Input name="email"/>
+        </Form.Item>
+        <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your password!',
+            },
+            ]}
+            hasFeedback
+        >
+            <Input.Password name="password"/>
+        </Form.Item>
+        <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+            {
+                required: true,
+                message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+                validator(rule, value) {
+                if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                }
+                return Promise.reject('The two passwords that you entered do not match!');
+                },
+            }),
+            ]}
+        >
+            <Input.Password />
+        </Form.Item>
+        <Form.Item
+            name="nickname"
+            label={
+            <span>
+                Nickname&nbsp;
+                <Tooltip title="What do you want others to call you?">
+                <QuestionCircleOutlined />
+                </Tooltip>
+            </span>
+            }
+            rules={[
+            {
+                required: true,
+                message: 'Please input your nickname!',
+                whitespace: true,
+            },
+            ]}
+        >
+            <Input name="nickname"/>
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+            Register
+            </Button>
+        </Form.Item>
+        </Form>
+    );
 };
 
 export default RegisterForm;
